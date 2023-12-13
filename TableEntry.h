a9c287c06500d357b1c857e77ce309323f7d094f
ESEMFER <esemfer@alumno.upv.es@ldsic-vdi03.upvnet.upv.es>
2023-12-13 18:29:54 +0100

    Añadida implementación de la clase TableEntry

--- TableEntry.h
+++ TableEntry.h
@@ -9,12 +9,36 @@ class TableEntry {
     public:
         std::string key;
 	V value = 0;
-	TableEntry(std::string key, V value);
-    	TableEntry(std::string key);
-	TableEntry();
-	friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2){return false;};
-	friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2){return false;};
-	friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te){return out;};
+
+	TableEntry(std::string key, V value){
+		this->key = key;
+		this->value = value;
+	}
+
+    	TableEntry(std::string key){
+		this->key = key;
+		value = 0;
+	}
+
+	TableEntry(){
+		key = "";
+		value = 0;
+	}
+
+	friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2){
+		if (te1.key == te2.key) { return true;}
+		else { return false;}
+	}
+
+	friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2){
+		if (te1.key == te2.key) {return false;}
+		else { return true;}
+	}
+
+	friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te){
+		out << "Entrada: " << te.key << " --> " << te.value;
+		return out;	
+	}
 };
 
 #endif
